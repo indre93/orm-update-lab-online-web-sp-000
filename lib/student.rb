7e943ef293036addedcd0dffd605807ed8a64bb0
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 00:45:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,10 +49,8 @@ class Student
  end
 
  def self.new_from_db(row)
-  student = Student.new
-  student.id = row[0]
-  student.name = row[1]
-  student.grade = row[2]
+  student = self.new(row[0], row[1], row[2])
+
   student
  end
 
