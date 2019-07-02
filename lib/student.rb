4ade085aae73a904337b00dfafd6d2fbba6d942c
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 00:28:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,10 @@ def self.find_by_name(name)
   end.first
 end
 
+def update 
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+end
 
 
 end
