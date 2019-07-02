b53e0b06a232196f7ddd8cbb8996b07efc7b7eb6
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-02 00:17:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,18 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+  sql =  <<-SQL
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+      SQL
+  DB[:conn].execute(sql)
+end
+
+
 
 
 
